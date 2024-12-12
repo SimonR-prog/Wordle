bbef2ffd67745c0c19f75a4359fb659c483b390b
Simon <152021717+SimonR-prog@users.noreply.github.com>
2024-12-13 00:54:47 +0100

    Added some comments of what might need to be added

---
+++ index.js
@@ -0,0 +1,15 @@
+//Need to import the array of words.
+
+//Need to make a variable with the amount of objects in the array.
+
+//Need to make a function that gets a random index from the array and loads the object into a variable.
+
+
+
+
+
+
+//Need function that blocks numbers and special characters.
+
+//Need a comparison thingy which will check each letter if it is the same as the user input and then turns the square.
+
